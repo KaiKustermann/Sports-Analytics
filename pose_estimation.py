65b8e90a3a8096026fdfb8850f42281bdaabe6ba
KaiKustermann <kai.kuster@gmx.de>
2022-05-22 18:02:36 +0200

    rename

--- pose_estimation.py
+++ pose_estimation.py
@@ -52,17 +52,6 @@ def write_landmarks_to_csv(folder_path, mp4_file_name):
 
       if count == 1:
         print(landmark_df)
-
-      ## Draw the pose annotation on the image.
-      # image.flags.writeable = True
-      # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-      # mp_drawing.draw_landmarks(
-      #     image,
-      #     results.pose_landmarks,
-      #     mp_pose.POSE_CONNECTIONS,
-      #     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-
-      # cv2.imshow('MediaPipe Pose', image)
       
       count = count + 1
       if cv2.waitKey(5) & 0xFF == 27:
