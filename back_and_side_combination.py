e609c96b9bc27204401e09879c3866a0d3634f5b
KaiKustermann <kai.kuster@gmx.de>
2022-05-25 19:35:47 +0200

    combine back and site function

--- back_and_side_combination.py
+++ back_and_side_combination.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-#only y coordinates or all coordinates????????
+# adjusts y coordinates of one dataframe to the other using mean
 def adjust_y_coordinates(landmark_back_df, landmark_site_df):
     for column in landmark_back_df.columns:
         if column[-1:] == "y":
@@ -11,10 +11,20 @@ def adjust_y_coordinates(landmark_back_df, landmark_site_df):
             landmark_site_df[column] += delta_mean
     return landmark_back_df, landmark_site_df
 
-def combine_angles(landmark_back_df, landmark_site_df, filename):
-    landmark_combined_df = (landmark_back_df + landmark_site_df) / 2
-    landmark_combined_df.to_csv("./landmark_results_combined/" + filename + ".csv")
-    return landmark_combined_df
+# combines two dataframes (back and site video) into one dataframe using the x coordinates of back and site and combining the y coordinates to one feature using the mean
+def combine_angles(landmark_back_df, landmark_site_df):
+    df_combined = pd.DataFrame()
+    for column in landmark_back_df.columns:
+        if column[-1:] == "x":
+            df_combined[column + "_back"] = landmark_back_df[column]
+    for column in landmark_site_df.columns:
+        if column[-1:] == "x":
+            df_combined[column + "_site"] = landmark_site_df[column]
+    for column in landmark_back_df.columns:
+        if column[-1:] == "y":
+            df_combined[column + "_combined"] = (landmark_back_df[column] + landmark_site_df[column]) / 2
+
+    return df_combined
 
 
 def combine_back_and_side_results(directory_str):
@@ -32,7 +42,10 @@ def combine_back_and_side_results(directory_str):
             landmark_site_df = pd.read_csv("./landmark_results/{}.csv".format(filename_site_edited), index_col=0)
             landmark_back_df = adjust_y_coordinates(landmark_back_df, landmark_site_df)[0]
             landmark_site_df = adjust_y_coordinates(landmark_back_df, landmark_site_df)[1]
-            landmark_combined_df = combine_angles(landmark_back_df, landmark_site_df, filename_edited[:-1] + "_combined")
+            landmark_combined_df = combine_angles(landmark_back_df, landmark_site_df)
+            landmark_combined_df.to_csv("./landmark_results_combined/{}.csv".format(filename_edited[:-1] + "_combined"))
             continue
         else:
             continue
+
+combine_back_and_side_results("./landmark_results")
